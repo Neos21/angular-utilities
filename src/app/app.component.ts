3abf9260eab9f24d4fc634b2b7ec78f7152abfe6
Neos21 <neos21@gmail.com>
2017-10-15 12:07:45 +0900

    Modify the development environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:no-console */
+
 import { Component, OnInit } from '@angular/core';
 
 /**
@@ -10,61 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   /** CoinHive mining status */
-  isWorking = false;
-
+  isWorking: boolean = false;
+  
   /** CoinHive miner */
-  protected miner;
-
+  protected miner: any;
+  
   /**
    * ngOnInit
    */
   ngOnInit(): void {
     if(!(window as any).CoinHive) {
       console.log('ngOnInit() : CoinHive does not exist.');
+      
       return;
     }
-
+    
     console.log('ngOnInit() : Setup CoinHive miner.');
     this.miner = new (window as any).CoinHive.User('jbVDoIh3MSdKF3wdHzSBu4pGM1mpI4sR', 'angular-utilities', {
       autoThreads: true,  // experimental
       throttle: 0.4       // 0 = 100% working
     });
   }
-
+  
   /**
    * Start mining
    */
   start(): void {
     if(!this.miner) {
       console.log('start() : CoinHive miner does not exist.');
+      
       return;
     }
-
+    
     if(this.isWorking) {
       console.log('start() : The miner is already working. Stop the miner...');
       this.stop();
     }
-
+    
     console.log('start() : Start mining.');
     this.miner.start();
     this.isWorking = true;
   }
-
+  
   /**
    * Stop mining
    */
   stop(): void {
     if(!this.miner) {
       console.log('stop() : CoinHive miner does not exist.');
+      
       return;
     }
-
+    
     if(!this.isWorking) {
       console.log('stop() : The miner is not working.');
       this.isWorking = false;
+      
       return;
     }
-
+    
     console.log('stop() : Stop mining.');
     this.miner.stop();
     this.isWorking = false;
