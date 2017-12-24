0aebbb966d1bf085d08245a12708a85b710147fd
Neos21 <neos21@gmail.com>
2017-12-24 23:14:04 +0900

    Modify Touch Friendly DnD And More...

--- src/app/pages/touch-friendly-dnd/touch-friendly-dnd/touch-friendly-dnd.component.ts
+++ src/app/pages/touch-friendly-dnd/touch-friendly-dnd/touch-friendly-dnd.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Touch Friendly DnD
+ * 
+ * PC でもスマホでも向いているのは @swimlane/ngx-dnd だった。
+ * 内部的には Dragula が使われている。
+ */
 @Component({
   selector: 'app-touch-friendly-dnd',
   templateUrl: './touch-friendly-dnd.component.html',
   styleUrls: ['./touch-friendly-dnd.component.scss']
 })
 export class TouchFriendlyDndComponent {
-  public items: Array<string> = ['1st', '2nd', '3rd', '4th', '5th'];
-  public dataList: Array<string> = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+  /** 適当なリスト 1 */
+  public items1: Array<string> = ['1st', '2nd', '3rd', '4th', '5th'];
+  
+  /** 適当なリスト 2 */
+  public items2: Array<string> = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
 }
