f1eac9218fd5a7bd00053f7d62ed9caa3f483cf0
Neos21 <neos21@gmail.com>
2018-06-14 00:17:49 +0900

    Add Multiple Diff

--- src/app/pages/multiple-diff/multiple-diff-routing.module.ts
+++ src/app/pages/multiple-diff/multiple-diff-routing.module.ts
@@ -11,43 +11,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MultipleDiffRoutingModule {
-  public texts: any[] = [
-    {
-      fileName: 'a.txt',
-      rawText: `{
-  "name": "project-a",
-  "version": "1.0.0",
-  "dependencies": {
-    "@neos21/neos21": "0.0.0"
-  }
-}`
-    },
-    {
-      fileName: 'b.txt',
-      rawText: `{
-  "name": "project-b",
-  "version": "1.0.0",
-  "dependencies": {
-    "@neos21/ccc": "0.0.2",
-    "@neos21/neos21": "0.0.0"
-  }
-}`
-    },
-    {
-      fileName: 'c.txt',
-      rawText: `{
-  "name": "project-c",
-  "private": true,
-  "version": "1.0.0",
-  "dependencies": {
-    "@neos21/neos21": "0.0.0"
-  }
-}`
-    }
-  ];
-  
-  public onDiff(): void {
-    
-  }
-}
+export class MultipleDiffRoutingModule { }
--- src/app/pages/multiple-diff/multiple-diff/multiple-diff.component.ts
+++ src/app/pages/multiple-diff/multiple-diff/multiple-diff.component.ts
@@ -1,68 +1,162 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Multiple Diff
+ * 
+ * 3ファイル以上比較できる。
+ */
 @Component({
   selector: 'app-multiple-diff',
   templateUrl: './multiple-diff.component.html',
   styleUrls: ['./multiple-diff.component.scss']
 })
 export class MultipleDiffComponent implements OnInit {
-  public texts: any[] = [
-    {
-      name: 'a.txt',
-      raw: `a
-{
+  /** 比較するデータ群 */
+  public texts: any[] = [];
+  
+  /**
+   * 初期表示時の処理
+   */
+  public ngOnInit(): void {
+    this.dummy();
+  }
+  
+  /** 空の列を追加する */
+  public addColumn(): void {
+    this.texts.push({
+      name: '',
+      raw: ''
+    });
+    this.execDiff();
+  }
+  
+  /** ダミーデータをセットする */
+  public dummy(): void {
+    const dummy = [
+      {
+        name: 'project-a/package.json',
+        raw: `{
   "name": "project-a",
   "version": "0.0.1",
   "dependencies": {
     "@neos21/neos21": "0.0.0"
   }
 }`
-    },
-    {
-      name: 'b.txt',
-      raw: `{
-  "private": true,
+      },
+      {
+        name: 'project-b/package.json',
+        raw: `{
   "name": "project-b",
   "version": "0.0.1",
-{
+  "private": true,
   "dependencies": {
     "@neos21/ccc": "0.0.1",
     "@neos21/neos21": "0.0.0"
   }
 }`
-    },
-    {
-      name: 'c.txt',
-      raw: `{
-  "private": true,
+      },
+      {
+        name: 'project-c/package.json',
+        raw: `{
   "name": "project-c",
   "version": "0.0.2",
+  "private": true,
   "dependencies": {
     "@neos21/neos21": "0.0.0"
   }
 }`
-    }
-  ];
-
-  ngOnInit(): void {
-    this.diff();
-  }
-  
-  public diff(): void {
-    this.texts.forEach((text) => {
-      text.diffed = undefined;
-    });
-    
-    this.texts.forEach((text, index) => {
-      if(index !== this.texts.length - 1) {
-        this.setDiffFlags(index, index + 1);
       }
-    });
+    ];
     
-    this.group();
+    this.texts = dummy;
+    this.execDiff();
   }
   
-  protected setDiffFlags(aIndex: number, bIndex: number): void {
+  /** 全削除する */
+  public removeAll(): void {
+    this.texts = [];
+  }
+  
+  /** 指定の列を削除する */
+  public removeColumn(index: number): void {
+    this.texts.splice(index, 1);
+    this.execDiff();
+  }
+  
+  public loadFile(event: Event, index: number): void {
+    // ファイルがない場合は何もしない
+    if(!event || !event.target || !event.target['files'] || !event.target['files'].length) {
+      // ファイルアップロード欄をリセットする
+      this.texts[index].file = true;
+      setTimeout(() => {
+        this.texts[index].file = false;
+      }, 1);
+      
+      return;
+    }
+    
+    const file = event.target['files'][0];
+    console.log(file);
+    
+    // テキストでないファイルを除外する
+    // 「text を含む」で判定すると .md ファイル (type なし) や .json (application/json) などが除外されてしまうため明らかに NG なファイルのみ除外する
+    if(/image/.test(file.type) || /video/.test(file.type)) {
+      alert('テキスト形式のファイルを選択してください');
+      // ファイルアップロード欄をリセットする
+      this.texts[index].file = true;
+      setTimeout(() => {
+        this.texts[index].file = false;
+      }, 1);
+      
+      return;
+    }
+    
+    // ファイルリーダを用意する
+    const reader = new FileReader();
+    // ファイルを読み込んだらデータをセットする
+    reader.onload = () => {
+      this.texts[index].name = file.name;
+      this.texts[index].raw = reader.result;
+      this.execDiff();
+      // ファイルアップロード欄をリセットする
+      this.texts[index].file = true;
+      setTimeout(() => {
+        this.texts[index].file = false;
+      }, 1);
+    };
+    // ファイル読み込みを実行する
+    reader.readAsText(file);
+  }
+  
+  /**
+   * Diff を実行する
+   */
+  public execDiff(): void {
+    // Diff 結果をリセットする
+    this.texts.forEach((text) => {
+      text.diffResult = undefined;
+    });
+    // Diff を実行する
+    this.texts.forEach((text, index) => {
+      this.diff(index);
+    });
+  }
+  
+  /**
+   * 行ごとに比較する
+   * 
+   * @param aIndex ベースにするデータの添字
+   */
+  protected diff(aIndex: number): void {
+    // 比較対象のデータの添字
+    let bIndex = aIndex + 1;
+    
+    // 比較対象の添字が this.texts の範囲を超えている場合は先頭と比較する
+    if(bIndex === this.texts.length) {
+      bIndex = 0;
+    }
+    
+    // 対象のデータを取得する
     const aLines = this.getLines(aIndex);
     const bLines = this.getLines(bIndex);
     
@@ -83,17 +177,18 @@ export class MultipleDiffComponent implements OnInit {
         if (!isSearched && bLine.isDiffPrev && aLine.text === bLine.text) {
           // A から見た B・B から見た A でともに差分なしと設定する
           aLine.isDiffNext = bLine.isDiffPrev = false;
-          // 差分なしの行同士がどれだけ離れているかを保存する
+          // 差分なしの行同士がどれだけ離れているかを保存する (今のところ用途なし)
           aLine.offsetNext = aLine.rawIndex - bLine.rawIndex;
           bLine.offsetPrev = bLine.rawIndex - aLine.rawIndex;
+          // 検索済とする
           isSearched = true;
         }
       });
     });
     
-    // A の方は既に比較されている場合があるので Diff 結果をマージすべきか確認する
-    this.setDiffed(aIndex, aLines);
-    this.setDiffed(bIndex, bLines);
+    // Diff 結果をマージしながら設定する
+    this.setDiffResult(aIndex, aLines);
+    this.setDiffResult(bIndex, bLines);
   }
   
   /**
@@ -112,169 +207,18 @@ export class MultipleDiffComponent implements OnInit {
    * Diff 結果をセットする。適宜マージする
    * 
    * @param index 対象の this.texts の添字
-   * @param diffed セットしたい Diff 結果データ
+   * @param diffResult セットしたい Diff 結果データ
    */
-  protected setDiffed(index: number, diffed: any[]): void {
-    if(this.texts[index].diffed === undefined) {
+  protected setDiffResult(index: number, diffResult: any[]): void {
+    if(this.texts[index].diffResult === undefined) {
       // 未定義の場合は直接今回の Diff 結果をセットする
-      this.texts[index].diffed = diffed;
+      this.texts[index].diffResult = diffResult;
     }
     else {
       // 過去の Diff 結果と今回の Diff 結果をマージする
-      this.texts[index].diffed.forEach((line, lineIndex) => {
-        Object.assign(line, diffed[lineIndex]);
+      this.texts[index].diffResult.forEach((line, lineIndex) => {
+        Object.assign(line, diffResult[lineIndex]);
       });
     }
   }
-  
-  protected group(): void {
-    const offsetLine = { isOffsetLine: true };
-    
-    const diffedList = this.texts.map((text) => {
-      return text.diffed;
-    });
-    
-    // 先に group プロパティを作っておく
-    this.texts.forEach((text) => {
-      text.group = [];
-    });
-    
-    // 1ファイル操作
-    diffedList.forEach((diffed, diffedIndex) => {
-      const group = this.texts[diffedIndex].group;
-      
-      const sameLines = diffed.filter((line) => {
-        return line.isDiffPrev === false || line.isDiffNext === false;
-      });
-      
-      // group に同じ行の配列を入れる
-      sameLines.forEach((sameLine) => {
-        group.push([sameLine]);
-      });
-      
-      // この時点で group の先頭の配列に、1行目でないデータが入っている場合は、group の先頭にデータを追加する
-      if(group[0] && group[0][0] && group[0][0].isOffsetLine === undefined && group[0][0].rawIndex !== 0) {
-        console.log(this.texts[diffedIndex].name, '1行目でない', group[0][0]);
-        group.unshift([diffed[0]]);
-        // 同様に別のグループの先頭に空行を入れる
-        this.texts.forEach((text, textIndex) => {
-          if(textIndex === diffedIndex) {
-            return;
-          }
-          
-          if(text.group.length === 0) {
-            text.group.push([offsetLine]);
-          }
-        });
-      }
-      
-      // 行を挿入する group の添字
-      let currentGroupIndex = 0;
-      diffed.forEach((line, lineIndex) => {
-        // グループのその添字の配列を取得する
-        const currentGroup = group[currentGroupIndex];
-        // 挿入する行が、配列の最初の要素と同じデータの場合は、同一行があるグループなので挿入しない
-        if(Object.is(line, currentGroup[0])) {
-          return;
-        }
-        
-        // 挿入する行が、次のグループの配列の最初の要素と同じだった場合は、挿入先となる添字をインクリメントし、その行は挿入せず終わる
-        const nextGroup = group[currentGroupIndex + 1];
-        if(nextGroup && Object.is(line, nextGroup[0])) {
-          currentGroupIndex++;
-          return;
-        }
-        
-      });
-      
-      console.log('終わり', group);
-    });
-    
-  }
-  
-  /*
-  protected group(): void {
-    const offsetObj = { isOffset: true };
-    
-    const diffedList = this.texts.map((text) => {
-      return text.diffed;
-    });
-    
-    // オフセット位置に空のオフセット行データを入れる
-    const includedOffsetList = [];
-    diffedList.forEach((diffed, index) => {
-      const includedOffset = [];
-      diffed.forEach((line) => {
-        // 手前に差分があるようならオフセット行データを入れる
-        if((line.offsetNext !== undefined && line.offsetNext < 0) || (line.offsetPrev !== undefined && line.offsetPrev < 0)) {
-          includedOffset.push(offsetObj);
-        }
-        // 元の行データを入れる
-        includedOffset.push(line);
-      });
-      includedOffsetList.push(includedOffset);
-    });
-    
-    // テスト
-    this.texts.forEach((text, index) => {
-      text.includedOffset = includedOffsetList[index];
-    });
-    
-    // オフセット行データの登場ごとに配列化する
-    includedOffsetList.forEach((includedOffset, includedOffsetIndex) => {
-      const groups = [];
-      
-      // 1ファイルの操作
-      includedOffset.forEach((line, lineIndex) => {
-        // 行ごとの処理
-        if(lineIndex === 0) {
-          console.log('1行目 → グループ作成');
-          groups.push([]);
-        }
-        
-        if(line.isOffset) {
-          console.log('オフセット行 → グループ作成');
-          groups.push([]);
-          
-          return;
-        }
-        
-        groups[groups.length - 1].push(line);
-      });
-      
-      console.log(this.texts[includedOffsetIndex].name, groups);
-    });
-  }
-  */
 }
-
-/*
-
-diffedList.forEach((diffed, index) => {
-  const includedOffset = [];
-  let countOffsetNext = 0;
-  let countOffsetPrev = 0;
-  
-  diffed.forEach((line) => {
-    if(line.offsetNext !== undefined && line.offsetNext < 0) {
-      const absOffset = Math.abs(line.offsetNext) - countOffsetNext;
-      for (let i = 0; i < absOffset; i++) {
-        includedOffset.push(offsetLine);
-        countOffsetNext++;
-      }
-    }
-    else if(line.offsetPrev !== undefined && line.offsetPrev < 0) {
-      const absOffset = Math.abs(line.offsetPrev) - countOffsetPrev;
-      for (let i = 0; i < absOffset; i++) {
-        includedOffset.push(offsetLine);
-        countOffsetPrev++;
-      }
-    }
-    
-    includedOffset.push(line);
-  });
-  
-  this.texts[index].includedOffset = includedOffset;
-});
-
- */
